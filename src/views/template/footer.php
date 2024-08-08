7c36d408b8d84534bcc30d9567a5699cb77e7708
Dave Martins <davidmartins140102@gmail.com>
2024-08-08 17:30:32 -0300

    Projeto Cod3r registro de ponto

---
+++ src/views/template/footer.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <span>ENIAC</span>
+    <span><i class="icofont-heart text-danger mx-1"></i></span>
+    <span>David</span>
+</footer>
+<script src="assets/js/app.js"></script>
+</body>
+</html>
